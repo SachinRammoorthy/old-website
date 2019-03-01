2d2c7a9e0639effbe336e42dde45f70b0fd3cbf9
Sachin Rammoorthy <sachin.rammoorthy@gmail.com>
2019-03-01 23:36:23 +0530

    removed old stuff

--- public/js/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyDY7OkaELY-Hn0xBzgd0IknmCKDJf3-AQE",
-    authDomain: "lead-management-140b7.firebaseapp.com",
-    databaseURL: "https://lead-management-140b7.firebaseio.com",
-    projectId: "lead-management-140b7",
-    storageBucket: "lead-management-140b7.appspot.com",
-    messagingSenderId: "423528065106"
-  };
-  firebase.initializeApp(config);
